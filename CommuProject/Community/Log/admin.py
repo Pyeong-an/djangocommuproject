89ff235021b136be4a26ffb462225cda26cdbd23
Pyeong-an <pyeongankim5@gmail.com>
2019-11-05 19:37:42 +0900

    LogList

--- CommuProject/Community/Log/admin.py
+++ CommuProject/Community/Log/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import LogList
 # Register your models here.
+
+class LogListAdmin(admin.ModelAdmin):
+    list_display = ('charname', 'where', 'contents')
+
+admin.site.register(LogList,LogListAdmin)
--- CommuProject/Community/Log/models.py
+++ CommuProject/Community/Log/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class LogList(models.Model):
+    charname =  models.ForeignKey('Char.CharList',on_delete =models.CASCADE, verbose_name='캐릭터 이름')
+    where =  models.CharField(max_length=64,verbose_name='장소')
+    charcon =  models.CharField(max_length=64,verbose_name='콘')
+    password = models.CharField(max_length=64,verbose_name='비밀번호')
+    contents = models.TextField(verbose_name='내용')
+    write_dttm = models.DateTimeField(auto_now_add=True, verbose_name='작성 시간')
+
+    def __str__(self):
+        return self.charname
+
+    class Meta:
+        db_table = 'LogList'
+        verbose_name = verbose_name_plural = '로그'
